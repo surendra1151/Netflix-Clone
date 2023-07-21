b58f27a374e99c8045002e7263a799fc11eb4157
surendra1151 <surendrakalluri@Krishnas-MacBook-Pro.local>
2023-07-20 21:09:00 -0500

    now removed videjs

--- src/components/WatchPage/WatchPage.tsx
+++ src/components/WatchPage/WatchPage.tsx
@@ -95,7 +95,163 @@ const WatchPage = () => {
   if (!!videoJsOptions.width) {
     return (
       <Box position="relative">
-        <VideoJSPlayer options={videoJsOptions} onReady={handlePlayerReady} />
+        {/* <VideoJSPlayer options={videoJsOptions} onReady={handlePlayerReady} /> */}
+        {/* {playerRef.current && playerInitialized && ( */}
+          <Box
+            position="absolute"
+            top="0"
+            left="0"
+            right="0"
+            bottom="0"
+            w="full"
+            h="full"
+          >
+            <Box px={2} position="absolute" top="60px" left="10px">
+              <IconButton
+                aria-label="back-icon"
+                background="transparent"
+                _hover={{
+                  background: "none",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleGoBack()}
+                icon={
+                  <ArrowBackIcon color="white" width="44px" height="44px" />
+                }
+              />
+            </Box>
+            <Box
+              px={{ base: 1, sm: 2 }}
+              position="absolute"
+              bottom="20px"
+              left={0}
+              right={0}
+            >
+              {/* Seekbar */}
+              <Stack direction="row" alignItems="center" spacing={1} py="13px">
+                <PlayerSeekbar
+                  playedSeconds={playerState.playedSeconds}
+                  duration={playerState.duration}
+                  seekTo={handleSeekTo}
+                />
+              </Stack>
+              {/* end Seekbar */}
+              {/* Controller */}
+              <Flex justifyContent="space-between" alignItems="center">
+                {/* left controller */}
+                <Stack
+                  direction="row"
+                  spacing={{ base: 1, sm: 3, md: 4 }}
+                  alignItems="center"
+                >
+                  {!playerState.paused ? (
+                    <IconButton
+                      aria-label="pause-icon"
+                      background="transparent"
+                      _hover={{
+                        background: "none",
+                      }}
+                      onClick={() => playerRef.current?.pause()}
+                      icon={<MdPause size="44px" color="white" />}
+                    />
+                  ) : (
+                    <IconButton
+                      aria-label="play-icon"
+                      background="transparent"
+                      _hover={{
+                        background: "none",
+                      }}
+                      onClick={() => playerRef.current?.play()}
+                      icon={<MdPlayArrow size="44px" color="white" />}
+                    />
+                  )}
+                  <IconButton
+                    background="transparent"
+                    _hover={{
+                      background: "none",
+                    }}
+                    aria-label="forward-icon"
+                    icon={<MdForward10 size="44px" color="white" />}
+                  />
+                  <IconButton
+                    background="transparent"
+                    _hover={{
+                      background: "none",
+                    }}
+                    aria-label="replay-icon"
+                    icon={<MdReplay10 size="44px" color="white" />}
+                  />
+                  <VolumeControllers
+                    muted={playerState.muted}
+                    handleVolumeToggle={() => {
+                      playerRef.current?.muted(!playerState.muted);
+                      setPlayerState((draft) => {
+                        return { ...draft, muted: !draft.muted };
+                      });
+                    }}
+                    value={playerState.volume}
+                    handleVolume={handleVolumeChange}
+                  />
+                  <Text fontSize="xs" color="white">
+                    {`${formatTime(playerState.playedSeconds)} / ${formatTime(
+                      playerState.duration
+                    )}`}
+                  </Text>
+                </Stack>
+                {/* end left controller */}
+                {/* middle time */}
+                <Flex alignItems="center">
+                  <Text
+                    noOfLines={1}
+                    fontSize="20px"
+                    textAlign="center"
+                    color="white"
+                    maxWidth="300px"
+                  >
+                    Title - Description
+                  </Text>
+                </Flex>
+                {/* end middle time */}
+                {/* right controller */}
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  spacing={{ base: 1, sm: 3, md: 4 }}
+                >
+                  <IconButton
+                    aria-label="settings-icon"
+                    background="transparent"
+                    _hover={{
+                      background: "none",
+                    }}
+                    color="white"
+                    icon={<SettingsIcon boxSize="40px" />}
+                  />
+                  <IconButton
+                    aria-label="view-icon"
+                    background="transparent"
+                    _hover={{
+                      background: "none",
+                    }}
+                    icon={
+                      <MdOutlineBrandingWatermark size="44px" color="white" />
+                    }
+                  />
+                  <IconButton
+                    aria-label="view-icon"
+                    background="transparent"
+                    _hover={{
+                      background: "none",
+                    }}
+                    icon={<MdFullscreen size="44px" color="white" />}
+                  />
+                </Stack>
+                {/* end right controller */}
+              </Flex>
+              {/* end Controller */}
+            </Box>
+          </Box>
+        {/* )} */}
       </Box>
     );
   }

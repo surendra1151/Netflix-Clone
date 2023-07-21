acdc1e8dc8e6c58756bb4ebb32451eba639568a8
surendra1151 <surendrakalluri@Krishnas-MacBook-Pro.local>
2023-07-20 22:27:27 -0500

    updated videJs code as mentioned in website

--- src/components/WatchPage/VideoJSPlayer.tsx
+++ src/components/WatchPage/VideoJSPlayer.tsx
@@ -16,38 +16,62 @@ export default function VideoJSPlayer({
   const playerRef = useRef<Player | null>(null);
 
   useEffect(() => {
-    (async function handleVideojs() {
-      if (!playerRef.current) {
-        const videoElement = document.createElement("video-js");
-        videoRef.current?.appendChild(videoElement);
-        const player = (playerRef.current = videojs(
-          videoElement,
-          options,
-          () => {
-            onReady && onReady(player);
-          }
-        ));
-      } else {
-        const player = playerRef.current;
-            // Check if the player is playing
-            if (!player.paused()) {
-                // Pause the player
-                await player.pause();
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      const videoElement = document.createElement("video-js");
 
-                // Then change width and height
-                player.width(options.width);
-                player.height(options.height);
-                
-                // Then play it again if needed
-                player.play();
-            } else {
-                // If it's not playing, just change width and height
-                player.width(options.width);
-                player.height(options.height);
-            }
-      }
-    })();
+      videoElement.classList.add('vjs-big-play-centered');
+      videoRef?.current?.appendChild(videoElement);
+
+      const player = playerRef.current = videojs(videoElement, options, () => {
+        videojs.log('player is ready');
+        onReady && onReady(player);
+      });
+
+    // You could update an existing player in the `else` block here
+    // on prop change, for example:
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
   }, [options, videoRef]);
+
+  // useEffect(() => {
+  //   (async function handleVideojs() {
+  //     if (!playerRef.current) {
+  //       const videoElement = document.createElement("video-js");
+  //       videoRef.current?.appendChild(videoElement);
+  //       const player = (playerRef.current = videojs(
+  //         videoElement,
+  //         options,
+  //         () => {
+  //           onReady && onReady(player);
+  //         }
+  //       ));
+  //     } else {
+  //       const player = playerRef.current;
+  //           // Check if the player is playing
+  //           if (!player.paused()) {
+  //               // Pause the player
+  //               await player.pause();
+
+  //               // Then change width and height
+  //               player.width(options.width);
+  //               player.height(options.height);
+                
+  //               // Then play it again if needed
+  //               player.play();
+  //           } else {
+  //               // If it's not playing, just change width and height
+  //               player.width(options.width);
+  //               player.height(options.height);
+  //           }
+  //     }
+  //   })();
+  // }, [options, videoRef]);
 
   useEffect(() => {
     const player = playerRef.current;
